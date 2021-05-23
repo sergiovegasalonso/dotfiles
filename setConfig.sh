773e5df15a96fa666a05d3881e24c081fd52b5fe
Sergio Vega Alonso <sergio.vegas.alonso@gmail.com>
2021-05-23 13:03:50 +0200

    First commit.

---
+++ setConfig.sh
@@ -0,0 +1,5 @@
+
+ln -s ~/dotfiles/kglobalshortcutsrc ~/.config/kglobalshortcutsrc
+ln -s ~/dotfiles/locale.conf ~/.config/locale.conf
+ln -s ~/dotfiles/.profile ~/.profile
+ln -s ~/dotfiles/.xprofile ~/.xprofile
