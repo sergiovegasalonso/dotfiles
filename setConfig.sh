ab965cd6b3c2abfb67e89e3b7d00aca9b9e0dcd7
Sergio Vega Alonso <sergio.vegas.alonso@gmail.com>
2021-06-08 18:14:11 +0200

    Config symbolic links updated

--- setConfig.sh
+++ setConfig.sh
@@ -4,3 +4,4 @@ ln -s ~/dotfiles/locale.conf ~/.config/locale.conf
 ln -s ~/dotfiles/.profile ~/.profile
 ln -s ~/dotfiles/.xprofile ~/.xprofile
 ln -s ~/dotfiles/.config/qtile/config.py ~/.config/qtile/config.py
+ln -s ~/dotfiles/.config/qtile/img ~/.config/qtile/img
